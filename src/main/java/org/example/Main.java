4a4de2a8b7fb15b37ba0247f2bb3a028a004f342
Milko Yanakiev <myanakiev@gmail.com>
2024-10-09 14:16:02 +0300

    Initial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> alphabetList = Arrays.asList("a", "b", "c", "d", "e");
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/org/example/service/LibraryServiceTest.java
@@ -0,0 +1,46 @@
+package org.example.service;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryServiceTest {
+
+    private static final Faker faker = new Faker();
+
+    private LibraryService libraryService;
+    private int currentYear;
+
+    @BeforeEach
+    public void setUp() {
+        libraryService = new LibraryService();
+        currentYear = LocalDate.now().getYear();
+    }
+
+    @Test
+    public void testCreateAuthorAndBookInvalidInput() {
+        // Arrange
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        int age = faker.random().nextInt(20, 80);
+        String title = faker.name().title();
+        int year = currentYear + 5;
+        int issn = faker.random().nextInt(1, 100);
+
+        // Act and Assert
+        try {
+            libraryService.createAuthorAndBook(firstName, lastName, age, title, year, issn);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Year in the future", e.getMessage());
+        }
+    }
+
+
+
+}
